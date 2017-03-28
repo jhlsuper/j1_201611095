d355d417fa0e7fd1bd2888c320352a2b45f60923
이재현201611095 <jhlsuper@gmail.com>
2017-03-28 11:50:51 +0900

    Add files via upload

---
+++ src/headfirst/strategy/MinDuckSimulation1.java
@@ -0,0 +1,14 @@
+package headfirst.strategy;
+
+public class MinDuckSimulation1{
+  public static void main(String[] args) {
+    Duck mallard = new MallardDuck();
+    //inmallard.performQuack();
+    mallard.performFly();
+    
+    
+    
+    
+  }
+}
+    
