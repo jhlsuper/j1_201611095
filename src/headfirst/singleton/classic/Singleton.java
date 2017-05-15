c3fd5e276a341a113090fd4e25c409a76c6fbc8a
이재현201611095 <jhlsuper@gmail.com>
2017-05-15 13:44:55 +0900

    Add files via upload

---
+++ src/headfirst/singleton/classic/Singleton.java
@@ -0,0 +1,33 @@
+package headfirst.singleton.classic;
+
+public class Singleton {
+  
+  private static Singleton uniqueInstance;
+  private static int numCalled=0;
+  
+  private Singleton() {
+  
+  }
+  public static Singleton getinstance() {
+    if(uniqueInstance ==null) {
+      System.out.println("creating ,,,");
+      uniqueInstance = new Singleton();
+    }
+    //else {
+    System.out.println("returning ,,,");
+    System.out.println("num called..."+numCalled++);
+      return uniqueInstance;
+    //}
+  }
+  //public void fill(){
+   // if(isEmpty()) {
+   //   System.out.println("filling,,");
+   //   empty=false;
+   //   boiled = false;
+    }
+  
+  //public boolean isEmpty() {
+  //  return empty;
+  
+
+  
