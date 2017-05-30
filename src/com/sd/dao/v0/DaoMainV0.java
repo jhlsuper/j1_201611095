8a8c66cce36f3646b03ea983eecbbff318eb1226
이재현201611095 <jhlsuper@gmail.com>
2017-05-30 14:44:16 +0900

    Add files via upload

--- src/com/sd/dao/v0/DaoMainV0.java
+++ src/com/sd/dao/v0/DaoMainV0.java
@@ -6,6 +6,7 @@ import java.io.*;
 public class DaoMainV0 {
   public static void main(String[] args) {
     Connection conn=null;
+    ResultSet rs=null;
     Statement stmt =null;
     try {
       String path = System.getProperty("user.dir");
@@ -19,15 +20,24 @@ public class DaoMainV0 {
              prop.getProperty("USER"),
              prop.getProperty("PASSWORD")
          );
+      
+      //String mySql="CREATE TABLE persons(id int(64) NOT NULL AUTO_INCREMENT,";
+      //mySql+="name varchar(255) NOT NULL, address varchar(255) NOT NULL, UNIQUE (id))";
+      //String mySql = "INSERT INTO persons(name,address) VALUES('v0-1','1 Hongji Dong')";
+      String mySql ="select name from persons where id<4 order by id";
       stmt = conn.createStatement();
-      String mySql="CREATE TABLE persons(id int(64) NOT NULL AUTO_INCREMENT,";
-      mySql+="name varchar(255) NOT NULL, address varchar(255) NOT NULL, UNIQUE (id))";
-      stmt.execute(mySql);
+      //stmt.execute(mySql);
+      rs=stmt.executeQuery(mySql);
+      while(rs.next()) {
+        System.out.println(rs.getString("name"));
+      }
+      
     
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
       try{
+      if(rs !=null) rs.close();
       if(stmt != null) stmt.close();
       if(conn != null) conn.close();
       }catch(Exception e) {};
